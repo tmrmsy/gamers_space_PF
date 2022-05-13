cef92aba3b968a235d37124fa3c77cb9838ec898
tmrmsy <gmtmrmsy@gmail.com>
2022-05-13 13:45:09 +0000

    [Add]Userモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :admin do
     get 'homes/top'
     get 'homes/about'
@@ -8,4 +9,3 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
- 
