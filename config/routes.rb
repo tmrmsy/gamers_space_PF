b7a0fe7d303c641a3434317784f1689cbe12b63d
tmrmsy <gmtmrmsy@gmail.com>
2022-05-16 15:47:53 +0000

    [Add]投稿一覧画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,29 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'posts/new'
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/withdrawal'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :admin do
-    resources :users, only: [:show, :edit, :update]
-  end
-  namespace :admin do
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  #devise_for :users
+
   devise_for :users, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
+
+  namespace :public do
+    resources :posts, only: [:new, :index, :show, :edit]
+  end
+  namespace :public do
+    resources :users, only: [:show, :edit]
+    get 'users/withdrawal'
+  end
+  namespace :public do
+    root to: 'homes#top'
+    get 'homes/about'
+  end
+
+  namespace :admin do
+    resources :users, only: [:show, :edit, :update]
+  end
+
+  namespace :admin do
+    resources :posts, only: [:index, :show, :edit]
+  end
 
   namespace :admin do
     root to: 'homes#top'
