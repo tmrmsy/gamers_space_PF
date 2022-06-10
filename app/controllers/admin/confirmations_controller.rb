7b430266b53e9d10b34ea0d5d04ee7b83a5c31c6
tmrmsy <gmtmrmsy@gmail.com>
2022-06-10 16:33:08 +0000

    [Fix]

--- app/controllers/admin/confirmations_controller.rb
+++ app/controllers/admin/confirmations_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Admin::Admin::ConfirmationsController < Devise::ConfirmationsController
+class Admin::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation/new
   # def new
   #   super
