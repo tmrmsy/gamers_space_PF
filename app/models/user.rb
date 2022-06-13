fc5ee910a9c60368ce158e92f80ead4eb826be1c
tmrmsy <gmtmrmsy@gmail.com>
2022-06-13 12:37:29 +0000

    [Fix]バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,15 @@ class User < ApplicationRecord
   has_many :entries, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :post_comments, dependent: :destroy
-  
+
 
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
   has_many :following_user, through: :follower, source: :followed
   has_many :follower_user, through: :followed, source: :follower
+
+  validates :name, uniqueness: true, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
